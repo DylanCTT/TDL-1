96de3dbc21e2bc7e2a0b436a09edc7e3a89fa5d6
DylanCTT <clattdylan@gmail.com>
2025-06-03 15:27:00 -0300

    Prac 5

--- Parctica/P5/Ej5/main.c
+++ Parctica/P5/Ej5/main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE *f;
+    f= fopen("diccionario.txt", "r");
+    char str[80];
+    char act[80];
+    if (f == NULL){
+        printf ("\nError al abrir archivo fuente\n");
+        return 1;
+    }
+    printf(" Ingrese la palabra que quiere buscar: ");
+    scanf(" %s", str);
+    fscan(f, "%s\n;", act);
+    while (!feof(f) && strcmp(str, 'FIN')!=0){
+
+    }
+
+
     return 0;
 }
